5901684eb4bf94941737842d5a44066dbb085e26
Akseliya <41054600+Akseliya@users.noreply.github.com>
2022-06-25 20:12:50 +0300

    добавлен task_5

---
+++ task_5.py
@@ -0,0 +1,24 @@
+from itertools import combinations_with_replacement
+import math
+
+
+def count_find_num(primesL, limit):
+    min_num = max_num = math.prod(primesL)
+    if min_num > limit:
+        return []
+
+    count_num = 1
+    per_len = 1
+    while True:
+        combs = tuple(combinations_with_replacement(primesL, per_len))
+        if math.prod((min_num,) + combs[0]) > limit:
+            break
+        for comb in combs:
+            comb_prod = math.prod((min_num,) + comb)
+            if comb_prod > limit:
+                continue
+            count_num += 1
+            max_num = max(max_num, comb_prod)
+        per_len += 1
+
+    return [count_num, max_num]
