816864965f88c8e131947da20644c23723d600bd
Akseliya <41054600+Akseliya@users.noreply.github.com>
2022-06-27 03:02:03 +0300

    изменён task_3

--- task_3.py
+++ task_3.py
@@ -1,15 +1,9 @@
 def zeros(n):
     count_5 = n // 5
-
     pow_5 = 25
-    count_pow = 1
     while pow_5 <= n:
         tmp_n = 1
-        while pow_5 * tmp_n <= n:
-            if tmp_n % 5:
-                count_5 += count_pow
-            tmp_n += 1
+        count_5 += n // pow_5
         pow_5 *= 5
-        count_pow += 1
 
     return count_5
